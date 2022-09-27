1df2e45cb4ff53c13bcc89ea44541e61a30816d8
Suneel Sarswat <46070433+suneel-sarswat@users.noreply.github.com>
2022-09-27 19:57:34 +0530

    updated_tail
    list_fold is used to do tail recursion on certain parts.

--- application/Checker.ml
+++ application/Checker.ml
@@ -29,9 +29,17 @@ let string_to_tuple line=create_instruction (String.split_on_char ',' line);;
 
 
 (**4. This function takes a list of strings and converts them into list of instructions*)
-let rec str_list_to_instructions mylist= match mylist with
+(*let rec str_list_to_instructions mylist= match mylist with
         | [] ->[]
-        | line::mylist' ->(string_to_tuple line)::(str_list_to_instructions mylist')
+        | line::mylist' ->(string_to_tuple line)::(str_list_to_instructions mylist') *)
+        
+(** using map **) (**f := (string_to_tuple) and l := mylist  map:= str_list_to_instructions **)
+let map f l = 
+  let rec map_aux acc = function
+    | [] -> acc []
+    | x :: xs -> map_aux (fun ys -> acc (f x :: ys)) xs
+  in
+  map_aux (fun ys -> ys) l
 
 
 (**5. Read input data and converts them into list of instructions**)
@@ -41,26 +49,35 @@ let rec str_list_to_instructions mylist= match mylist with
 
 
 
-let rec printmfile oc (m:Extracted.transaction list) k = match m with 
+let rec printmfile oc (m:Extracted.transaction list) = match m with 
   |[] -> ()
   |[f] -> Printf.fprintf oc "%d,%d,%d\n" f.idb f.ida f.tquantity
-  |f::m' -> Printf.fprintf oc "%d,%d,%d;" f.idb f.ida f.tquantity; printmfile oc m' k
+  |f::m' -> Printf.fprintf oc "%d,%d,%d;" f.idb f.ida f.tquantity; printmfile oc m' 
 
-let writematching file (m:Extracted.transaction list) k =
+let writematching file (m:Extracted.transaction list) =
   let oc = open_out file in
-  printmfile oc m k ;
+  printmfile oc m ;
   close_out oc;;
 
 
+
+let ploop p lst oc =
+	let combiner acc x =
+		let output = p (Extracted.blist acc) (Extracted.alist acc) x in 
+		(printmfile oc (Extracted.mlist output)); output
+	in
+	let init = (([],[]),[])
+	in
+	List.fold_left combiner init lst ;;
+(**
 let rec ploop p i k oc=
   (fun zero succ n -> if n=0 then zero () else succ (n-1))
     (fun _ -> let output = (([],[]),[]) in (printmfile oc (Extracted.mlist output) 0); output )
     (fun k' ->
     let output = (let it = ploop p i k' oc in
-    p (Extracted.blist it) (Extracted.alist it) (Extracted.nth k' i Extracted.tau0))  in (printmfile oc (Extracted.mlist output) (k'+1)); output   ) 
+    p (Extracted.blist it) (Extracted.alist it) (Extracted.nth k' i Extracted.tau0).cmd (Extracted.nth k' i Extracted.tau0).ord)  in (printmfile oc (Extracted.mlist output) (k'+1)); output   ) 
     k 
-
-
+**)
 
 (**Comparison part**)
 
@@ -94,7 +111,7 @@ let string_to_transaction_list line = str_list_to_trnsactions (String.split_on_c
 (**3. This function takes a list of strings and converts them into list of transactions*)
 let rec printm (m:Extracted.transaction list) str = match m with
 	|[] -> ()
-	|[a] -> (Printf.printf "This is %s output: Buy id: %i, Sell id: %i, Quantity: %i \n" str a.idb a.ida a.tquantity)
+	|[a] -> (Printf.printf "This is %s matching: Buy id: %i, Sell id: %i, Quantity: %i \n" str a.idb a.ida a.tquantity)
 	|a::m' -> (Printf.printf "Buy id: %i, Sell id: %i, Quantity: %i\n" a.idb a.ida a.tquantity);printm m' str;;
 
 (**Now apply string_to_transaction_list recursivelly on each line of the two files parrallely. Then use b= compare_matchings with cform(m1) and cform(m2)
@@ -119,12 +136,16 @@ let rec compare_outputs l1 l2 k = match (l1, l2) with
 let iorderfile = Sys.argv.(1) ;;
 let itradefile = Sys.argv.(2) ;;
 
-let orderbook = str_list_to_instructions (lines_from_files iorderfile)
+let orderbook = map (string_to_tuple) (lines_from_files iorderfile );;
 let m = Extracted.length orderbook;;
+
 Printf.printf "\n%s contains %d instructions.\n" iorderfile m;;
 let ofile = "verified_" ^ Sys.argv.(2) ;;
+
+
+
 let () = let oc = open_out ofile in
-ignore (ploop (Extracted.process_instruction) orderbook m oc) ; close_out oc;;
+ignore (ploop (Extracted.process_instruction) orderbook oc) ; close_out oc;;
 
 Printf.printf "\nVerified transactions succesfully created and saved in %s.\n\n" ofile ;;
 Printf.printf "Compared transactions in %s with transactions in %s.\n\n" ofile itradefile ;;
